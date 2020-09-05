f16fb7436fc26bd657302718dbbdb6f35fa4ecd5
Rutenis Turcinas <rutenis.turcinas@gmail.com>
2020-09-05 11:17:07 +0300

    ModuleEntity and Repository

---
+++ src/main/java/lt/sdacademy/university/repositories/ModuleRepository.java
@@ -0,0 +1,21 @@
+package lt.sdacademy.university.repositories;
+
+import java.util.Collection;
+import java.util.List;
+import lt.sdacademy.university.models.entities.ModuleEntity;
+import org.springframework.data.repository.Repository;
+
+public interface ModuleRepository extends Repository<ModuleEntity, Long> {
+
+    List<ModuleEntity> findAll();
+
+    List<ModuleEntity> findAllByTitle(String title);
+
+    List<ModuleEntity> findAllByTitleNot(String title);
+
+    List<ModuleEntity> findAllByTitleIn(Collection<String> titles);
+
+    List<ModuleEntity> findAllByTitleLike(String title);
+
+    List<ModuleEntity> findAllByTitleContains(String title);
+}
