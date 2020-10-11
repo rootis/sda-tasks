57df5984a02a51b950bc1b808e2491e02496804b
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2020-10-11 10:13:35 +0300

    Code formatting

--- src/test/java/lt/sdacademy/university/repositories/PersonRepositoryTest.java
+++ src/test/java/lt/sdacademy/university/repositories/PersonRepositoryTest.java
@@ -1,6 +1,8 @@
 package lt.sdacademy.university.repositories;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.security.InvalidParameterException;
 import java.sql.SQLException;
