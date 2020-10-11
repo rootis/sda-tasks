619ca99a149831bcff74b9e659f4d7b5cb186229
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2020-10-11 13:04:15 +0300

    Optimize PersonRepository

--- src/main/java/lt/sdacademy/university/repositories/PersonRepository.java
+++ src/main/java/lt/sdacademy/university/repositories/PersonRepository.java
@@ -1,50 +1,46 @@
 package lt.sdacademy.university.repositories;
 
 import java.util.List;
+import java.util.function.Consumer;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import lt.sdacademy.university.models.entities.PersonEntity;
 import lt.sdacademy.university.services.ConnectionService;
 
 public class PersonRepository {
 
-    private final EntityManagerFactory entityManagerFactory;
+    private final EntityManager em;
 
     public PersonRepository() {
-        entityManagerFactory = ConnectionService.getSessionFactory();
+        em = ConnectionService.getSessionFactory().createEntityManager();
     }
 
     public List<PersonEntity> findAll() {
         String hqlQuery = "select p from PersonEntity p";
-        return entityManagerFactory.createEntityManager().createQuery(hqlQuery, PersonEntity.class).getResultList();
+        return em.createQuery(hqlQuery, PersonEntity.class).getResultList();
     }
 
     public PersonEntity findOne(Long id) {
-        return entityManagerFactory.createEntityManager().find(PersonEntity.class, id);
+        return em.find(PersonEntity.class, id);
     }
 
     public PersonEntity save(PersonEntity person) {
-        EntityManager em = entityManagerFactory.createEntityManager();
-        EntityTransaction transaction = em.getTransaction();
-        transaction.begin();
-        try {
-            em.persist(person);
-            transaction.commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-            transaction.rollback();
-        }
-
-        return person;
+        return persist(person, em::persist);
     }
 
     public PersonEntity update(PersonEntity person) {
-        EntityManager em = entityManagerFactory.createEntityManager();
+        return persist(person, em::merge);
+    }
+
+    public void delete(PersonEntity person) {
+        persist(person, em::remove);
+    }
+
+    private PersonEntity persist(PersonEntity person, Consumer<PersonEntity> consumer) {
         EntityTransaction transaction = em.getTransaction();
         transaction.begin();
         try {
-            em.merge(person);
+            consumer.accept(person);
             transaction.commit();
         } catch (Exception e) {
             e.printStackTrace();
@@ -52,18 +48,5 @@ public class PersonRepository {
         }
 
         return person;
-    }
-
-    public void delete(PersonEntity person) {
-        EntityManager em = entityManagerFactory.createEntityManager();
-        EntityTransaction transaction = em.getTransaction();
-        transaction.begin();
-        try {
-            em.remove(person);
-            transaction.commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-            transaction.rollback();
-        }
     }
 }
