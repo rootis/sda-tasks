7f0f8a833c8d6cab7401895c29d38e4b39ace144
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2020-10-11 12:37:45 +0300

    PersonRepository CRUD

--- src/main/java/lt/sdacademy/university/repositories/PersonRepository.java
+++ src/main/java/lt/sdacademy/university/repositories/PersonRepository.java
@@ -1,7 +1,9 @@
 package lt.sdacademy.university.repositories;
 
 import java.util.List;
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import lt.sdacademy.university.models.entities.PersonEntity;
 import lt.sdacademy.university.services.ConnectionService;
 
@@ -16,5 +18,52 @@ public class PersonRepository {
     public List<PersonEntity> findAll() {
         String hqlQuery = "select p from PersonEntity p";
         return entityManagerFactory.createEntityManager().createQuery(hqlQuery, PersonEntity.class).getResultList();
+    }
+
+    public PersonEntity findOne(Long id) {
+        return entityManagerFactory.createEntityManager().find(PersonEntity.class, id);
+    }
+
+    public PersonEntity save(PersonEntity person) {
+        EntityManager em = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        try {
+            em.persist(person);
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            transaction.rollback();
+        }
+
+        return person;
+    }
+
+    public PersonEntity update(PersonEntity person) {
+        EntityManager em = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        try {
+            em.merge(person);
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            transaction.rollback();
+        }
+
+        return person;
+    }
+
+    public void delete(PersonEntity person) {
+        EntityManager em = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        try {
+            em.remove(person);
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            transaction.rollback();
+        }
     }
 }
--- src/test/java/lt/sdacademy/university/repositories/PersonRepositoryTest.java
+++ src/test/java/lt/sdacademy/university/repositories/PersonRepositoryTest.java
@@ -1,9 +1,12 @@
 package lt.sdacademy.university.repositories;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.sql.SQLException;
 import java.util.List;
+import lt.sdacademy.university.models.Gender;
 import lt.sdacademy.university.models.entities.PersonEntity;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,5 +25,40 @@ class PersonRepositoryTest {
         List<PersonEntity> result = personRepository.findAll();
 
         assertFalse(result.isEmpty());
+    }
+
+    @Test
+    void findOne() {
+        PersonEntity result = personRepository.findOne(1L);
+
+        assertNotNull(result);
+    }
+
+    @Test
+    void save() {
+        PersonEntity person = new PersonEntity();
+        person.setName("Test Name");
+        person.setSurname("Test Surname");
+        person.setGender(Gender.male);
+
+        person = personRepository.save(person);
+
+        assertNotNull(person.getId());
+        personRepository.delete(person);
+    }
+
+    @Test
+    void delete() {
+        Integer oldSize = personRepository.findAll().size();
+        PersonEntity person = new PersonEntity();
+        person.setName("Test Name");
+        person.setSurname("Test Surname");
+        person.setGender(Gender.male);
+
+        person = personRepository.save(person);
+        assertEquals(oldSize + 1, personRepository.findAll().size());
+
+        personRepository.delete(person);
+        assertEquals(oldSize, personRepository.findAll().size());
     }
 }
