28ce23c0866f5249237cea901c3ee2ddd4c95569
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2020-10-11 11:52:44 +0300

    UniversityEntity

---
+++ src/main/java/lt/sdacademy/university/models/entities/UniversityEntity.java
@@ -0,0 +1,23 @@
+package lt.sdacademy.university.models.entities;
+
+public class UniversityEntity {
+
+    private String code;
+    private String title;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
