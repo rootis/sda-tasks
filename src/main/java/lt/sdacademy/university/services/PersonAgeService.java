784b749af3f394c1e323f364cb9d4c36b2c8071f
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2020-12-06 09:27:16 +0200

    Task

---
+++ src/main/java/lt/sdacademy/university/services/PersonAgeService.java
@@ -0,0 +1,13 @@
+package lt.sdacademy.university.services;
+
+import java.util.List;
+import lt.sdacademy.university.models.dto.Person;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonAgeService {
+
+    public Person getOldestPerson(List<Person> people) {
+        return null;
+    }
+}
