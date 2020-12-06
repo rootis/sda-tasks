c3d1376822c8fec0d58d3050a4e1c4b49f1f64b2
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2020-12-06 10:56:37 +0200

    Person to map

--- src/main/java/lt/sdacademy/university/services/PersonAgeService.java
+++ src/main/java/lt/sdacademy/university/services/PersonAgeService.java
@@ -1,6 +1,10 @@
 package lt.sdacademy.university.services;
 
+import static java.util.stream.Collectors.groupingBy;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import lt.sdacademy.university.models.dto.Person;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +13,16 @@ public class PersonAgeService {
 
     public Person getOldestPerson(List<Person> people) {
         return null;
+    }
+
+    public Map<String, List<Person>> getPeopleByName(List<Person> people) {
+        return people.stream().collect(groupingBy(Person::getName));
+    }
+
+    public Map<String, Double> getAverageNameAge(List<Person> people) {
+        Map<String, Double> result = new HashMap<>();
+        getPeopleByName(people).forEach((key, value) -> result.put(key, value.stream().mapToDouble(Person::getAge).average().orElse(0)));
+
+        return result;
     }
 }
