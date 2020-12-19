f9b0d00e70d499a8576115f92f5db10ca4dbc1cd
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2020-12-19 13:49:51 +0200

    User DTO

--- src/main/java/lt/sdacademy/university/models/dto/User.java
+++ src/main/java/lt/sdacademy/university/models/dto/User.java
@@ -2,9 +2,25 @@ package lt.sdacademy.university.models.dto;
 
 public class User {
 
+    private Long id;
     private String email;
     private String password;
     private String name;
+
+    public User(Long id, String email, String password, String name) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getEmail() {
         return email;
