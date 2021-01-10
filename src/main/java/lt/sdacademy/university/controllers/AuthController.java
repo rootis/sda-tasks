7d5c888276b2e59da8d02228f15089a6a43ae869
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2021-01-10 11:57:18 +0200

    CrossOrigin

--- src/main/java/lt/sdacademy/university/controllers/AuthController.java
+++ src/main/java/lt/sdacademy/university/controllers/AuthController.java
@@ -8,10 +8,12 @@ import lt.sdacademy.university.services.SecurityService;
 import lt.sdacademy.university.validators.UserValidator;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class AuthController {
 
