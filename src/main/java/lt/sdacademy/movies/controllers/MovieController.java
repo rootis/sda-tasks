2fe303826cd5fde1e75771d6a714e4bc17e26564
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2021-01-24 13:09:05 +0200

    MovieController and initial MovieService

--- src/main/java/lt/sdacademy/movies/controllers/MovieController.java
+++ src/main/java/lt/sdacademy/movies/controllers/MovieController.java
@@ -1,6 +1,48 @@
 package lt.sdacademy.movies.controllers;
 
+import java.util.List;
+import javax.validation.Valid;
+import lt.sdacademy.movies.models.dto.Movie;
+import lt.sdacademy.movies.services.MovieService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/movies")
 public class MovieController {
 
+    private final MovieService movieService;
 
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping
+    public List<Movie> getAll() {
+        return movieService.getMovies();
+    }
+
+    @GetMapping("/{id}")
+    public Movie getMovie(@PathVariable Long id) {
+        return movieService.getMovie(id);
+    }
+
+    @PostMapping
+    public Movie save(@RequestBody @Valid Movie movie) {
+        return movieService.save(movie);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        movieService.delete(id);
+
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
 }
---
+++ src/main/java/lt/sdacademy/movies/services/MovieService.java
@@ -0,0 +1,8 @@
+package lt.sdacademy.movies.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MovieService {
+
+}
