d75bd8b60051e7880e021f8364ab72616d506f3e
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2021-01-24 11:23:52 +0200

    Initial setup for tests

---
+++ src/test/java/lt/sdacademy/movies/repositories/MovieRepositoryIT.java
@@ -0,0 +1,22 @@
+package lt.sdacademy.movies.repositories;
+
+import org.junit.jupiter.api.Test;
+
+class MovieRepositoryIT {
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
